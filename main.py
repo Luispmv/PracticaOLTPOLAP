abd471d4f9a0548a6cfaa5f12f01752cb1f74fc2
Luispmv <luispmv07@hotmail.com>
2025-10-18 09:26:51 -0600

    Transformaciones de 1 a 3

--- main.py
+++ main.py
@@ -50,3 +50,51 @@ print("Tabla salidas_det:", df_salidas_det.shape)
 print("\nExtraccion completada!")
 
 
+
+# TRANSFORMACION1: Crear dimension de libros (dim_libros)
+# Combinar libros + autor + editorial
+
+dim_libros = df_libros.merge(df_autor, on='idautor', how='left')
+dim_libros = dim_libros.merge(df_editorial, left_on='ideditorial', right_on='idedit', how='left')
+
+# Seleccionar y renombrar columnas segun estructura OLAP
+dim_libros = dim_libros[['idlibro', 'nom_libro', 'idautor', 'nom_autor', 'ideditorial', 'nom_edit']]
+dim_libros.rename(columns={'nom_edit':'nom_editorial'}, inplace=True)
+
+print("Dimension libros creada:")
+print(dim_libros.head())
+print("Registros:", len(dim_libros))
+
+
+
+# TRANSFORMACION 2: Crear dimension de salidas (dim_salidas)
+
+dim_salidas = df_salidas_det[['idsalida', 'total_cant']].copy()
+
+print("Dimension salidas creada: ")
+print(dim_salidas)
+print("Rgistros:", len(dim_salidas))
+
+
+
+# TRANSFORMACION 3: Crear dimension de tiempos (dim_tiempos)
+# Extraer year, month, day de la fecha
+
+dim_tiempos = df_salidas_det[['fecha_sal']].copy()
+dim_tiempos.rename(columns={'fecha_sal':'idtiempo'}, inplace=True)
+
+#Convertir a datetime si no no lo es
+dim_tiempos['idtiempo'] = pd.to_datetime(dim_tiempos['idtiempo'])
+
+#Extraer componentes de fecha
+dim_tiempos['year_sal'] = dim_tiempos['idtiempo'].dt.year
+dim_tiempos['month_sal'] = dim_tiempos['idtiempo'].dt.month
+dim_tiempos['day_sal'] = dim_tiempos['idtiempo'].dt.day
+
+print("Dimension tiempos creada:")
+print(dim_tiempos)
+print("Registros: ", len(dim_tiempos))
+
+
+
+
